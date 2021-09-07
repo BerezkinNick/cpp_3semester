cd1807f22b91984ce37b6e109426b4c410b2d556
BerezkinNick <71658698+BerezkinNick@users.noreply.github.com>
2021-09-08 00:54:07 +0300

    Убрал повторные описания методов

--- Image/Image_test.cpp
+++ Image/Image_test.cpp
@@ -15,7 +15,7 @@ public:
 
     Image(int* raw, int W, int H, int D):
             H{H}, W{W}, D{D}, image{vector<vector<vector<int>>>
-                                            (H, vector<vector<int>> (W, vector<int> (D, 0)))}
+    (H, vector<vector<int>> (W, vector<int> (D, 0)))}
     {
         for(int i = 0; i < H; ++i){
             for(int j=0; j < W; ++j){
@@ -25,12 +25,13 @@ public:
             }
         }
     }
+
     Image(): Image(0, 0, 0){}
     Image(int W, int H, int D){
         vector<vector<vector<int>>> image(H, vector<vector<int>> (W, vector<int> (D, 0)));
     }
-    
-        int height(){
+
+    int height(){
         return H;
     }
 
@@ -45,12 +46,12 @@ public:
     Image(Image& other){ H = other.height(); W = other.width(); D = other.depth();
         image = other.image;
     }
-    
+
     int at(int x, int y, int d){
-            return image[y][x][d];
+        return image[y][x][d];
     }
-    
-        void print()
+
+    void print()
     {
         int im=0;
         for (size_t i = 0; i < H; ++i){
@@ -77,22 +78,13 @@ public:
 
         }
     }
-    
-
-    Image(Image& other){ H = other.height(); W = other.width(); D = other.depth();
-        image = other.image;
-    }
-
-    int at(int x, int y, int d){
-            return image[y][x][d];
-    }
 
     void crop(int x1, int y1, int x2, int y2)
     {
-        int H2 = y2 - y1;
-        int W2 = x2 - x1;
+        int H2 = y2-y1;
+        int W2 = x2-x1;
         vector < vector < vector<int > > > image2(y2-y1+1, vector<vector<int>>
-                (x2 + 1 - x1, vector<int> (D, 0)));
+                                                                            (x2-x1+1, vector<int> (D, 0)));
 
         for (size_t i = 0; i < H2 + 1; ++i){
             for (size_t j=0; j < W2 + 1; ++j){
@@ -104,32 +96,6 @@ public:
         H = H2 + 1;
         W = W2 + 1;
         image = image2;
-    }
-
-    void print()
-    {
-        int im=0;
-        for (size_t i = 0; i < H; ++i){
-            for (size_t j = 0; j < W; ++j){
-                for (size_t t = 0; t < D; ++t){
-                    im = im + image[i][j][t];
-                } int p;
-                p = im / D;
-                cout <<p<<' ';
-                cout <<' ';
-                int x = 0;
-                while (p > 0){
-                    x = x + 1;
-                    p = (p/10) - 1;
-                }
-                for (int i = 0; i <= 2 - x; ++i){
-                    cout <<' ';
-                }
-                im = 0;
-            }
-            cout << '\n';
-
-        }
     }
 
     void to_gs()
